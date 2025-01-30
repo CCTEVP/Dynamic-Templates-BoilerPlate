6ab6e10949e9e203a085bd28bf8143526076ed44
Erick Valencia Perea <126554851+CCTEVP@users.noreply.github.com>
2025-01-30 12:17:20 +0000

    Replit Adaptation

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/', // Adjust this path as needed
-    publicDir: 'public',
+    base: "/", // Adjust this path as needed
+    publicDir: "public",
+    server: {
+        host: "0.0.0.0",
+    },
 });
